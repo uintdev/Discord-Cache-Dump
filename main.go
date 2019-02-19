195e7250c4ebe4b6361a2f39e1d6222fc72e8336
Andre Santos <NodePoint@users.noreply.github.com>
2019-02-19 16:53:36 +0000

    Tester's release 5
    Appropriate amount of newlines when the program ends are now added. Sudoer name is now able to be displayed. Avoid adding an excessive newline when there is no cache present at all. Changed the root message for GNU/Linux users.

--- main.go
+++ main.go
@@ -21,16 +21,27 @@ const (
 
 var platform = runtime.GOOS
 
+// Add extra line of space when the program exits
+func exitNewLine() string {
+	var endOfProgram string
+	if platform == "windows" {
+		endOfProgram = "\n"
+	} else {
+		endOfProgram = "\n\n"
+	}
+	return endOfProgram
+}
+
 // Check for root, as running the program with it can change the path
 func rootCheck(tuid int) {
 	if tuid == 0 {
 		if platform != "darwin" {
-			fmt.Print("[WARN] This program is running as root\n")
-			fmt.Print("[...] Unless you are logged in as root, this will not check with the actual user you have logged in as.\n\n")
+			fmt.Print("[NOTICE] This program is running as root\n")
+			fmt.Print("[...] The logged in user will be used.\n\n")
 		}
 	} else {
 		if platform == "darwin" {
-			fmt.Print("[ERROR] Due to file permissions, this must be ran as root on macOS\n")
+			fmt.Printf("[ERROR] Due to file permissions, this must be ran as root on macOS%s", exitNewLine())
 			os.Exit(1)
 		}
 	}
@@ -98,18 +109,18 @@ func main() {
 	fmt.Print("\n")
 	// Banner
 	fmt.Print("###########################################################\n")
-	fmt.Printf("# Discord Cache Dump :: Version %0.1f :: TESTER'S RELEASE 4 #\n", softVersion)
+	fmt.Printf("# Discord Cache Dump :: Version %0.1f :: TESTER'S RELEASE 5 #\n", softVersion)
 	fmt.Print("###########################################################\n\n")
 
 	user, err := user.Current()
 	if err != nil {
-		fmt.Printf("[ERROR] Failed to obtain user: %s\n", err)
+		fmt.Printf("[ERROR] Failed to obtain user: %s%s", err, exitNewLine())
 		os.Exit(1)
 	} else {
 		if platform != "windows" {
 			uidConv, err := strconv.Atoi(user.Uid)
 			if err != nil {
-				fmt.Print("[ERROR] Unable to obtain UID\n")
+				fmt.Printf("[ERROR] Unable to obtain UID%s", exitNewLine())
 				os.Exit(1)
 			}
 			uid = uidConv
@@ -122,9 +133,11 @@ func main() {
 
 	if platform != "windows" {
 		// Get the sudoer user
-		userName, ok := os.LookupEnv("SUDO_USER")
+		userNamei, ok := os.LookupEnv("SUDO_USER")
 		if !ok {
 			userName = user.Username
+		} else {
+			userName = userNamei
 		}
 
 		// Get the sudoer UID
@@ -134,7 +147,7 @@ func main() {
 		} else {
 			sudoerUID, err = strconv.Atoi(sudoerUIDi)
 			if err != nil {
-				fmt.Print("[ERROR] Unable to convert UID to INT\n")
+				fmt.Printf("[ERROR] Unable to convert UID to INT%s", exitNewLine())
 				os.Exit(1)
 			}
 		}
@@ -157,11 +170,11 @@ func main() {
 		// Windows does not return just the username, so we split and grab what we need
 		userName = strings.Split(userName, "\\")[1]
 	} else {
-		fmt.Printf("[ERROR] Unsupported platform: %s\n", platform)
+		fmt.Printf("[ERROR] Unsupported platform: %s%s", platform, exitNewLine())
 		os.Exit(1)
 	}
 
-	fmt.Printf("User used to execute this program: %s\n\n", userName)
+	fmt.Printf("Logged in as: %s\n\n", userName)
 
 	fmt.Print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
 	fmt.Print("!! A few files (index, data_0-3) are in use by Discord while    !!\n")
@@ -204,8 +217,6 @@ func main() {
 	// Check if directories exist
 	for i := 0; i < len(discordBuildDir); i++ {
 		filePath := fmt.Sprintf(cachePath[platform], homePath, discordBuildDir[i])
-		fmt.Printf("DEBUG: %s\n", filePath) // DEBUG
-
 		if _, err := os.Stat(filePath); !os.IsNotExist(err) {
 			fmt.Printf("Found: Discord %s\n", discordBuildName[i])
 			pathStatus[i] = true
@@ -214,15 +225,13 @@ func main() {
 		}
 	}
 
-	fmt.Print("\n")
-
 	// Check if the directories are empty and store names of cached files
 	for i := 0; i < len(discordBuildDir); i++ {
 		if pathStatus[i] {
 			filePath := fmt.Sprintf(cachePath[platform], homePath, discordBuildDir[i])
 			cacheListing, err := ioutil.ReadDir(filePath)
 			if err != nil {
-				fmt.Printf("[ERROR] Unable to read directory for Discord %s\n", discordBuildName[i])
+				fmt.Printf("[ERROR] Unable to read directory for Discord %s%s", discordBuildName[i], exitNewLine())
 				os.Exit(1)
 			}
 			// Go through the list of files present in a cache directory
@@ -240,8 +249,6 @@ func main() {
 		}
 	}
 
-	fmt.Print("\n")
-
 	// If there are still no results at all, there is no point in continuing
 	pathStatusSuccessCount := 0
 	for i := 0; i < len(discordBuildDir); i++ {
@@ -250,14 +257,16 @@ func main() {
 		}
 	}
 	if pathStatusSuccessCount == 0 {
-		fmt.Print("No cache found\n")
+		fmt.Printf("No cache found%s", exitNewLine())
 		os.Exit(0)
+	} else {
+		fmt.Print("\n")
 	}
 
 	// Check storage requirements
 	curDir, err := os.Getwd()
 	if err != nil {
-		fmt.Print("[ERROR] Unable to obtain current directory\n")
+		fmt.Printf("[ERROR] Unable to obtain current directory%s", exitNewLine())
 		os.Exit(1)
 	}
 	freeStorage(curDir)
@@ -265,7 +274,7 @@ func main() {
 		remainingStorage := spareStorage - overallSize
 		requiredSpace := strings.Replace(fmt.Sprintf("%v", remainingStorage), "-", "", -1)
 		fmt.Print("[ERROR] Insufficient storage where program is being ran\n")
-		fmt.Printf("[...] %s bytes need sparing\n", requiredSpace)
+		fmt.Printf("[...] %s bytes need sparing%s", requiredSpace, exitNewLine())
 		os.Exit(1)
 	} else {
 		fmt.Print("Sufficient storage -- safe to go ahead\n\n")
@@ -342,9 +351,9 @@ func main() {
 
 	// Let user know where to get to the files
 	if platform == "windows" {
-		fmt.Printf("Saved: %s\\%s\\%s\n", curDir, dumpDir, timeDateStamp)
+		fmt.Printf("Saved: %s\\%s\\%s%s", curDir, dumpDir, timeDateStamp, exitNewLine())
 	} else {
-		fmt.Printf("Saved: %s/%s/%s\n", curDir, dumpDir, timeDateStamp)
+		fmt.Printf("Saved: %s/%s/%s%s", curDir, dumpDir, timeDateStamp, exitNewLine())
 	}
 
 }
